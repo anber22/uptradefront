feb6e4a83db3695acde4b21b2d63255fb6b716e9
nuanyang233 <nuanyang233@gmail.com>
2022-07-11 23:19:23 +0800

    fix script

--- sh/qa.sh
+++ sh/qa.sh
@@ -10,5 +10,5 @@ pnpm run build
 
 rm -rf node_modules
 rm -rf build.tar.gz
-#tar --exclude=build.tar.gz  --exclude=.git --warning=no-file-changed -zcvf build.tar.gz ./
+tar --exclude=build.tar.gz  --exclude=.git --warning=no-file-changed -zcvf build.tar.gz ./
 echo 'success'
