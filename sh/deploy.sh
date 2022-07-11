0dc2e8a076b09a20dae8282f9392df8a29823053
nuanyang233 <nuanyang233@gmail.com>
2022-07-11 21:26:35 +0800

    fix: deploy sh

---
+++ sh/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ENV=$1
+# 启动 pm2
+pm2 flush
+pm2 stop uptrade
+pm2 delete uptrade
+pm2 start npm --name "uptrade" -- run start
+
+sudo /sbin/nginx -t
+sudo /sbin/nginx -s reload
+
+
+NGINX_CACHE_DIR="/usr/local/nginx/data"
+
+# look for empty dir
+if [ "$(ls -A $NGINX_CACHE_DIR)" ]; then
+     echo "$NGINX_CACHE_DIR 存在缓存文件，进行清理"
+     sudo rm -r /usr/local/nginx/data/*
+fi
+# echo '清理nginx缓存文件'
+# sudo rm -r /usr/local/nginx/data/*
