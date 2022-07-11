dd9551725e60f49b87f00c588c1f761a95affdda
nuanyang233 <nuanyang233@gmail.com>
2022-07-11 22:49:35 +0800

    fix: test build

--- sh/qa.sh
+++ sh/qa.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-node -v
-npm -v
+
+corepack enable
+corepack
+
+pnpm -v
+pnpm install
 rm -rf build.tar.gz
 rm -rf cache.json
-tar --exclude=build.tar.gz  --exclude=.git --warning=no-file-changed -zcvf build.tar.gz ./
+#tar --exclude=build.tar.gz  --exclude=.git --warning=no-file-changed -zcvf build.tar.gz ./
 echo 'success'
