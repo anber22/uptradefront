d9d98952c50c4065ad6260f27810d3c5ec47aefe
nuanyang233 <nuanyang233@gmail.com>
2022-07-15 16:17:21 +0800

    fix

--- sh/qa.sh
+++ sh/qa.sh
@@ -4,12 +4,15 @@ corepack enable
 ls
 rm -rf build.tar.gz
 rm cache.json
+rm -rf .next
+
 ls
 pnpm -v
 pnpm install --frozen-lockfile --prefer-offline
 pnpm run build
 
 rm -rf node_modules
+
 rm -rf build.tar.gz
 tar --exclude=build.tar.gz  --exclude=.git --warning=no-file-changed -zcvf build.tar.gz ./
 echo 'success'
