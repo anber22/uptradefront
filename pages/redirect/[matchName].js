93e83bebcc444725a44b2a1840ab2761677413f6
nuanyang233 <nuanyang233@gmail.com>
2022-07-18 17:53:49 +0800

    fix

--- pages/redirect/[matchName].js
+++ pages/redirect/[matchName].js
@@ -31,7 +31,7 @@ export default function Redirect() {
       },
       body: JSON.stringify({
         productId,
-        target: redirectUrl,
+        targetUrl: redirectUrl,
       }),
     }).finally(() => {
       const url = urlcat(redirectUrl, {
