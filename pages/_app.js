6c2acc72185dfd2db2c45d3d2203375062f8b3a9
anber22 <51233546+anber22@users.noreply.github.com>
2023-07-08 13:28:41 +0800

    合并代码

--- pages/_app.js
+++ pages/_app.js
@@ -60,7 +60,7 @@ function MyApp({ Component, pageProps }) {
           </>
         ) : null}
         {!isAmp ? (
-          <script async id="google-tag-manager" strategy="afterInteractive">
+          <script defer id="google-tag-manager" strategy="afterInteractive">
             {`
            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
